e3b45ffb66177f0fc3cb0b6f362c0ed97f2fbb4e
bwxzgeretou <bwxzgeretou@gmail.com>
2022-09-23 19:25:25 +0900

    introduce plug_vim

---
+++ install_plug.vim.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+# vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
