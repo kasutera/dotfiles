ecfe84bebdccef98a8393b64009f7a2298140a77
bwxzgeretou <bwxzgeretou@gmail.com>
2022-09-23 17:55:48 +0900

    add gitconfig

--- config.sh
+++ config.sh
@@ -1,13 +1,29 @@
-#!/bin/sh
-cd $(dirname $0)
+#!/usr/bin/env bash
+set -e
 
-mkdir -p ~/.vim
-mkdir -p ~/.vim_tmp
-mkdir -p ~/.config/nvim
+if [[ "$0" == ./* ]]; then
+    cd $(dirname $0)
+else
+    echo "ERROR: Please run this with ./config.sh" >&2
+    exit 1
+fi
 
-for filename in .vimrc .gvimrc .zshrc .tigrc .bashrc
+mkdir -p ~/.vim/
+mkdir -p ~/.vim_tmp/
+mkdir -p ~/.config/nvim/
+mkdir -p ~/.config/git/
+touch ~/.gitconfig.local
+
+for filename in \
+    .vimrc \
+    .gvimrc \
+    .zshrc \
+    .tigrc \
+    .bashrc \
+    .config/git/config
 do
-    ln -sf ${PWD}/${filename} ~
+    ln -sf "${PWD}/${filename}" ${HOME}/${filename}
 done
-ln -sf ${PWD}/.vimrc ~/.config/nvim/init.vim
+
+ln -sf "${PWD}/.vimrc" ~/.config/nvim/init.vim
 
